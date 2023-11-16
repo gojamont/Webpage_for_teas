e3f247939666cd35b4efb6eac50224df89f1e9e7
gojamont <goja.mont@gmail.com>
2023-11-17 00:27:34 +0200

    modified some files

--- app.py
+++ app.py
@@ -10,5 +10,21 @@ def index():
 def meniu():
     return render_template("meniu.html")
 
+@app.route('/vaistas_ir_maistas_visada_salia')
+def edu_1():
+    return render_template("vaistas_maistas_salia.html")
+
+@app.route('/vaistazoles_prie_namu')
+def edu_2():
+    return render_template("vaistazoles_prie_namu.html")
+
+@app.route('/bobvakariai')
+def edu_3():
+    return render_template("bobvakariai.html")
+
+@app.route('/kontaktai')
+def kontaktai():
+    return render_template("kontaktai.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
