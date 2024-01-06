3cbbda12f9880ee0637880e8a5907b65e28871b0
gojamont <goja.mont@gmail.com>
2024-01-06 16:20:06 +0200

    a bit modified favicon files

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_from_directory
+import os
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
 @app.route('/')
 def index():
     return render_template("index.html")
