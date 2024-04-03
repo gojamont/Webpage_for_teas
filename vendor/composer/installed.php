3ede80e5e5ffdaf65962757b7f6b89c5a3718ad2
gojamont <goja.mont@gmail.com>
2024-04-04 00:41:45 +0300

    added some files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gojam/renatossodai',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'deedcfb5f78581bc4250481dfbe37b9a45929f72',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gojam/renatossodai' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'deedcfb5f78581bc4250481dfbe37b9a45929f72',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
