5ccf0bdd5f492a3673438533d076b4168ece8bde
Goja Montvilaite <citrusiuke@srv504292.hstgr.cloud>
2024-04-12 20:19:09 +0000

    added a few changes

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def galerija():
     return render_template("galerija.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
